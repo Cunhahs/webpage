b4aa4f00b7f3774c7830ddaec92887e751ddb769
Henrique Cunha <70229768+Cunhahs@users.noreply.github.com>
2023-04-25 20:31:41 -0300

    configurando spring

--- src/main/java/br/com/senac/webpage/WebpageApplication.java
+++ src/main/java/br/com/senac/webpage/WebpageApplication.java
@@ -2,8 +2,9 @@ package br.com.senac.webpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
 public class WebpageApplication {
 
 	public static void main(String[] args) {
