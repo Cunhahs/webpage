5080a5aba516ad8112cc6fcd65079d9770586b78
Henrique Cunha <70229768+Cunhahs@users.noreply.github.com>
2023-04-25 19:52:33 -0300

    primeiro commit spring

---
+++ src/test/java/br/com/senac/webpage/WebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.webpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
