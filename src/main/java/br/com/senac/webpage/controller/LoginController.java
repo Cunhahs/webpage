634b4eb99de962e39a84cbbc8010348bfe1a20b2
Henrique Cunha <70229768+Cunhahs@users.noreply.github.com>
2023-04-25 23:38:08 -0300

    ligando js

--- src/main/java/br/com/senac/webpage/controller/LoginController.java
+++ src/main/java/br/com/senac/webpage/controller/LoginController.java
@@ -1,11 +1,20 @@
 package br.com.senac.webpage.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.google.gson.Gson;
+
+import br.com.senac.webpage.dao.UsuarioDAO;
 import br.com.senac.webpage.model.Usuario;
+import br.com.senac.webpage.util.Cripto;
 
 @Controller
 @RequestMapping("/login")
@@ -13,14 +22,24 @@ public class LoginController {
 
     @GetMapping
     public String init(final Usuario usuario) {
-    	System.out.println("bati");
-        return "paginaUsuarioBackoffice";
+    	return "paginaUsuarioBackoffice";
         
     }
     @PostMapping
-    public void requestLogin() {
+    public void requestLogin(JSONObject json) throws SQLException, NoSuchAlgorithmException, UnsupportedEncodingException {
+    	Gson gson = new Gson();
+    	gson.toJson(json);
+    	String senha = (String) json.get("senha");
+    	String email = (String) json.get("email");
+    	
     	System.out.println("bati");
-        
+    	UsuarioDAO usuarioDAO = new UsuarioDAO();
+    	Cripto cripto = new Cripto();
+    	String senhaFinal = cripto.crip(senha);
+    	boolean altorizado = usuarioDAO.validar(email, senhaFinal);
+    	if (altorizado == true)  System.out.println(senhaFinal);
+    		
+		
     }
    
 }
--- src/main/webapp/scriptPaginaUsuarioBackoffice.js
+++ src/main/webapp/scriptPaginaUsuarioBackoffice.js
@@ -1,4 +1,13 @@
-var JSONObject= {"uname":uname, "password":password };
+const sSenha = document.querySelector('#senhaLogar');
+const sEmail = document.querySelector('#emailLogar');
+const btnLogar = document.querySelector('#btnLogar');
+
+console.log("Olá !");
+
+btnLogar.onclick = e => { //botao de salvar os dados da inclusao
+  	
+  	console.log("Olá mundo!");
+var JSONObject= {"email":sEmail, "senha":sSenha};
 var jsonData = JSON.parse( JSONObject );    
 
 var request = $.ajax({
@@ -7,4 +16,7 @@ type: "POST",
 data: jsonData,
 dataType: "json"
 });  
+
+   
+}
 
