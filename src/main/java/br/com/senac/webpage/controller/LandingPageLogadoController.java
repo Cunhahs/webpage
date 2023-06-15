5446ce34ff3891fef35ecefa56b92fdf31cca7af
Henrique Cunha <70229768+Cunhahs@users.noreply.github.com>
2023-06-15 19:06:41 -0300

    add

--- src/main/java/br/com/senac/webpage/controller/LandingPageLogadoController.java
+++ src/main/java/br/com/senac/webpage/controller/LandingPageLogadoController.java
@@ -8,20 +8,14 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.senac.webpage.dao.ProdutoDAO;
-import br.com.senac.webpage.dao.UsuarioDAO;
-import br.com.senac.webpage.model.Produto;
 import br.com.senac.webpage.model.ProdutoAllDto;
-import br.com.senac.webpage.model.UsuarioDto;
-import br.com.senac.webpage.util.Cripto;
-import br.com.senac.webpage.util.IdSession;
 import br.com.senac.webpage.util.ListCarrinho;
 
 @Controller
@@ -41,13 +35,13 @@ public class LandingPageLogadoController {
         return mv;
     }
     
-    @PostMapping
-    public ModelAndView result(@ModelAttribute Produto produto) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    @GetMapping("/landingPageLogado/{nome}")
+    public ModelAndView result(@PathVariable("nome") String path)  throws NoSuchAlgorithmException, UnsupportedEncodingException {
     	System.out.println("Post landing page");
-    	System.out.println(produto.getDescricao());
+    	System.out.println(path);
     	
-    	if (produto!=null) {
-    		ListCarrinho.carrinho.add(produto.getDescricao());		
+    	if (path!=null) {
+    		ListCarrinho.carrinho.add(path);		
 		}
     
         	ModelAndView modelAndView = new ModelAndView("redirect:carrinhoCliente");
