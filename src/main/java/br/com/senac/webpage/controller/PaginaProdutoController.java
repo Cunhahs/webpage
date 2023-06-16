d6eb56135ba94a81d9540c43a475515eba849a8f
Alejandro <ale_huller@hotmail.com>
2023-06-16 10:39:27 -0300

    fazendo pagina do produto

--- src/main/java/br/com/senac/webpage/controller/PaginaProdutoController.java
+++ src/main/java/br/com/senac/webpage/controller/PaginaProdutoController.java
@@ -3,11 +3,13 @@ package br.com.senac.webpage.controller;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,27 +17,35 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.senac.webpage.dao.ProdutoDAO;
 import br.com.senac.webpage.dao.UsuarioDAO;
 import br.com.senac.webpage.model.Produto;
+import br.com.senac.webpage.model.ProdutoAllDto;
 import br.com.senac.webpage.model.UsuarioDto;
 import br.com.senac.webpage.util.Cripto;
+import br.com.senac.webpage.util.ListCarrinho;
 
 @Controller
-@RequestMapping("/paginaProdutos2")
+@RequestMapping("/paginaProduto")
 public class PaginaProdutoController {
 
     @GetMapping
-    public String init(final Model model) {
-    	//model.addAttribute("produto", new Produto());
-    	return "paginaProduto2";
+    public ModelAndView init(Model model) throws SQLException{
+    	ModelAndView mv = new ModelAndView();
+        ProdutoDAO produtoRepository = new ProdutoDAO();
+        List<ProdutoAllDto> produtos = produtoRepository.findAll();
+    	mv.addObject("produtos", produtos);
+        return mv;
         
     }
     
-    @PostMapping
-    public void result(@ModelAttribute Produto produto) throws NoSuchAlgorithmException, UnsupportedEncodingException, SQLException {
-    	System.out.println("salvar do produto");
-    	ProdutoDAO produtoDAO = new ProdutoDAO();
-    	//produtoDAO.inserir(produto);
+   @GetMapping("/{nome}")
+    public ModelAndView result(@PathVariable("nome") String path)  throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    	System.out.println();
     	
-    	
-    }
+    	System.out.println("Post landing page");
+    	System.out.println(path);
+    
+        	ModelAndView modelAndView = new ModelAndView("redirect:/paginaProduto");
+        	return modelAndView;	
+		
+    }     
     
 }
